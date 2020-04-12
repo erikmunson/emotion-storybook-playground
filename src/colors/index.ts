7c68380e11249e28eca98efb3a09b3e0a89decd1
Erik Munson <emunson@netflix.com>
2020-04-12 15:00:18 -0700

    Add Colors doc

--- src/colors/index.ts
+++ src/colors/index.ts
@@ -1,19 +1,22 @@
 export const statusColors = {
-  inactive: '#c0c0c0',
-  neutral: '#999999',
-  info: '#61b9ff',
-  progress: '#61b9ff',
-  success: '#64c800',
-  warning: '#ffad26',
-  error: '#ff8282',
+  standard: {
+    inactive: '#c0c0c0',
+    neutral: '#999999',
+    info: '#61b9ff',
+    progress: '#61b9ff',
+    success: '#64c800',
+    warning: '#ffad26',
+    error: '#ff8282',
+  },
 
-  inactiveLight: '#666666',
-  neutralLight: '#ebebeb',
-  infoLight: '#e6f0ff',
-  progressLight: '#e6f0ff',
-  successLight: '#e1f5d2',
-  warningLight: '#f8f8bb',
-  errorLight: '#ffe1e1',
+  light: {
+    neutral: '#ebebeb',
+    info: '#e6f0ff',
+    progress: '#e6f0ff',
+    success: '#e1f5d2',
+    warning: '#f8f8bb',
+    error: '#ffe1e1',
+  },
 } as const;
 
 export const iconColors = {
@@ -23,13 +26,17 @@ export const iconColors = {
 } as const;
 
 export const validationColors = {
-  error: '#CE0000',
-  info: '#0065b7',
-  success: '#007023',
-  warning: '#F7CE00',
+  standard: {
+    error: '#CE0000',
+    info: '#0065b7',
+    success: '#007023',
+    warning: '#F7CE00',
+  },
 
-  errorLight: '#FCDDDE',
-  infoLight: '#D8F2FA',
-  successLight: '#DBF1E3',
-  warningLight: '#FAECAC',
+  light: {
+    error: '#FCDDDE',
+    info: '#D8F2FA',
+    success: '#DBF1E3',
+    warning: '#FAECAC',
+  },
 } as const;
